953c0898c5b69cc60657cac97d4f65037d090453
Hayden Miller <hmiller320@gmail.com>
2021-01-20 14:12:20 -0600

    added NPM install

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const emoji = require("node-emoji");
 let coffee = emoji.get('coffee');
 console.log(coffee);
 
+/* 
 function emojiLoop = (array){
     for (let i = 0; i <= array.length; i++){
         return console.log(emoji[i])
     }
 
-}
+} 
+*/
