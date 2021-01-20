6f67eca236c33483b3222b816bd3ef4db9b234fa
Hayden Miller <hmiller320@gmail.com>
2021-01-20 14:23:48 -0600

    updated function

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const emoji = require("node-emoji");
 let coffee = emoji.get('coffee');
 console.log(coffee);
 
-/* 
-function emojiLoop = (array){
+
+function emojiLoop(array){
     for (let i = 0; i <= array.length; i++){
-        return console.log(emoji[i])
+        return console.log(emoji.get(array[i]));
     }
 
 } 
-*/
+
+emojiLoop(['coffee','pizza'])
